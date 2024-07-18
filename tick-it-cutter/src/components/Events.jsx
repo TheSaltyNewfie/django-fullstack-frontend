8d8b3ba43dcb5788324b973ca68ddc8b957c6542
sebcuellar6 <seabasscuellarh@gmail.com>
2024-07-18 14:06:28 -0500

    events/venues styled

--- tick-it-cutter/src/components/Events.jsx
+++ tick-it-cutter/src/components/Events.jsx
@@ -1,81 +1,74 @@
 import MainNavbar from "./Navbar";
 import React from "react";
-import {Container, Card, Button, Row, Col
-} from "react-bootstrap";
+import { Container, Card, Button, Row, Col } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import config from "../config/config";
 import axios from "axios";
 
 export default function Events() {
+  const [events, setEvents] = useState([]);
+  const [randomEvent, setRandomEvent] = useState(null);
 
-    const [events, setEvents] = useState([])
-    const [randomEvent, setRandomEvent] = useState(null)
-
-    useEffect(() => {
-
-    
-
+  useEffect(() => {
     const getData = async () => {
-        try {
-            const response = await axios.get(`http://localhost:8000/events/`)
-            console.log(response.data)
-            setEvents(response.data)
+      try {
+        const response = await axios.get(`http://localhost:8000/events/`);
+        console.log(response.data);
+        setEvents(response.data);
 
-            const randomIndex = Math.floor(Math.random() * response.data.length)
-            setRandomEvent(response.data[randomIndex])
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    getData()
-    }, [])
+        const randomIndex = Math.floor(Math.random() * response.data.length);
+        setRandomEvent(response.data[randomIndex]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
 
-    return (
-        <>
-        <MainNavbar/>
-      <div className="header">
-        <h1>Events</h1>
-      </div>
-      <Container className="mt-5">
+  return (
+    <>
+      <MainNavbar />
+      <Container className="mt-5 featured-event">
         {randomEvent && (
-      <Card className="p-5">
-        <Card.Body>
-          <Card.Title as="h1" className="display-4">Events</Card.Title>
-          <Card.Text className="lead">
-            Featured Event: {randomEvent.name}
-          </Card.Text>
-          <hr className="my-4" />
-          <Card.Text>
-            {randomEvent.artists.name}
-          </Card.Text>
-          <Button variant="primary" size="lg" href="#" role="button">Learn more</Button>
-        </Card.Body>
-      </Card>
-      )}
-    </Container>
-    <Container className="mt-4">
+          <Card className="p-5 featured-card">
+            <Card.Body>
+              <Card.Title as="h1" className="display-4 featured-title">Events</Card.Title>
+              <Card.Text className="lead featured-sub-title">
+                Featured Event: {randomEvent.name}
+              </Card.Text>
+              <hr className="my-4 featured-text" />
+              <Card.Text className="featured-artist">
+                {randomEvent.artists.name}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        )}
+      </Container>
+      <Container className="mt-4">
         <Row>
           {events.map((event) => (
-            <Col key={event.id} sm={12} md={6} lg={4}>
-              <Card className="m-2" style={{ width: '100%' }}>
-                <Row nogutters="true">
-                  <Col xs="auto">
-                    <Card.Img src="https://via.placeholder.com/150" style={{ height: '100%', margin: '1px' }}/>
-                  </Col>
-                  <Col>
-                    <Card.Body>
-                      <Card.Title>{event.name}</Card.Title>
-                      <Card.Text>Venue: {event.venue.name}</Card.Text>
-                      <Card.Text>Artists: {event.artists.name}</Card.Text>
-                      <Card.Text>Price of Entry: {event.entry_fee}</Card.Text>
-                    </Card.Body>
-                  </Col>
-                </Row>
-              </Card>
-            </Col>
+              <div className="event-card full-width-card">
+                <Card className="m-2 inner-card" style={{ width: '100%' }}>
+                  <Row noGutters="true">
+                    <Col xs="auto">
+                      <Card.Img className="event-image center-image" src="https://via.placeholder.com/150" />
+                    </Col>
+                    <Col>
+                      <Card.Body>
+                        <Card.Title className="event-title">{event.name}</Card.Title>
+                        <Card.Text className="event-text">Venue: {event.venue.name}</Card.Text>
+                        <Card.Text className="event-text">Artists: {event.artists.name}</Card.Text>
+                        <Card.Text className="event-text">Price of Entry: {event.entry_fee}</Card.Text>
+                      </Card.Body>
+                    </Col>
+                  </Row>
+                </Card>
+              </div>
+           
           ))}
         </Row>
       </Container>
-       </>
-    );
-  }
+    </>
+  );
+}
+
