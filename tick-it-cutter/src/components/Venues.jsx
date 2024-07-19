f95d9cc36a74f1e99116c69cc737b3eed7c493d0
lhinds86 <leonhinds1986@gmail.com>
2024-07-19 11:11:48 -0400

    CSS update

--- tick-it-cutter/src/components/Venues.jsx
+++ tick-it-cutter/src/components/Venues.jsx
@@ -5,13 +5,12 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Stack from 'react-bootstrap/Stack';
 import axios from "axios";
 import config from "../config/config";
 import { useState, useEffect } from "react";
 import './Event-Venue.css';
 import jay from '../assets/jay-z.jpg'
-
+import MadisonSG from '../assets/madison-square-garden.jpg'
 
 export default function Venues() {
 
@@ -34,44 +33,16 @@ getData()
     return (
         <>
             <MainNavbar />
-            <div className="filter">
-                <Stack gap={3}>
-                    <div className="p-2" id="filter1">
-                        <h1 style={{ color: 'black', textAlign: 'center' }}>Venues</h1>
+    
+                    <div className="venue-title">
+                        <h1>Venues</h1>
                     </div>
-                    <div className="p-2" id="filter2">
-                        <Navbar className="d-flex justify-content-center align-items-center">
-                            <Form>
-                                <Row>
-                                    <Col xs="auto">
-                                        <Form.Control
-                                            type="text"
-                                            placeholder="Search"
-                                            className="venue-search"
-                                        />
-                                    </Col>
-                                    <Col xs="auto">
-                                        <Button type="submit">Submit</Button>
-                                    </Col>
-                                </Row>
-                            </Form>
-                        </Navbar>
-                    </div>
-                    <div className="p-2" id="filter3">
-                        <div className="d-flex justify-content-between">
-                            <Button variant="primary">Venues</Button>{' '}
-                            <Button variant="primary">Venues</Button>{' '}
-                            <Button variant="primary">Venues</Button>{' '}
-                            <Button variant="primary">Venues</Button>{' '}
-                            <Button variant="primary">Venues</Button>{' '}
-                        </div>
-                    </div>
-                </Stack>
-            </div>
+            
+         
             <div className="card-container d-flex flex-wrap justify-content-center">
                     {venues.map((venue) => (
-                        <Card key={venue.name} className="m-2" style={{ width: '100%' }}>
-                            <Card.Img variant="top" src={jay} className=".card-image"/>
+                        <Card key={venue.name} className="m-2" style={{ width: '20%' }}>
+                            {/* <Card.Img variant="top" src={MadisonSG} className=".card-image" /> */}
                             <Card.Body>
                             <Card.Title className="card-title">Venue: {venue.name}</Card.Title>
                             <Card.Text className="card-text">Location: {venue.location}</Card.Text>
